385ca1ba62db2f952941a60f0a30424b74ec7086
Чернов Станислав Игоревич ИП-515 <starstars91@mail.ru>
2025-10-05 16:57:51 +0700

    Исправлен ввод ФИО

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char** argv) {
  char* str = (char*)malloc(sizeof(char) * SIZE);
  printf(Chernov Stas:\n");
- scanf("%s", str);
+ str = gets(str);
  printf("Привет, %s", str);
  return 0;
 }
